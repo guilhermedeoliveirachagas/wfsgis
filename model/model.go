47d86623790316468b89ee0717b0c05da093f77e
tingold <tingold@boundlessgeo.com>
2018-03-06 14:21:45 -0700

    in progress

--- model/model.go
+++ model/model.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	_ "github.com/lib/pq"
 	"github.com/boundlessgeo/wt/ogc"
+	json2 "encoding/json"
+	"github.com/paulmach/orb"
 )
 
 //DB holds the DB connection
@@ -95,6 +97,7 @@ func(d *DB) CreateCollectionTable(collectionName string, features []*ogc.Feature
 		log.Printf("Error creating table: %v",err)
 		return err
 	}
+
 	//makeGeom := "SELECT ST_AddGeometryColumn('public',$1,'geom',4326,'POINT',2)"
 	//_, err = d.db.Exec(makeGeom,collectionName)
 	//if err != nil {
@@ -104,6 +107,22 @@ func(d *DB) CreateCollectionTable(collectionName string, features []*ogc.Feature
 	return nil
 
 }
+
+func(d *DB) InsertFeature(collectionName string, features []*ogc.Feature){
+
+	//insert := "INSERT INTO $1 (geom, json) VALUES($2, $3)"
+	//
+	//for _,feature := range features{
+	//
+	//	json, _ := json2.Marshal(feature)
+	//	orb.AllGeometries.
+
+
+//	}
+
+
+}
+
 //gets features based on query
 func(d *DB) GetFeatures(request ogc.GetFeatureRequest) ([]*ogc.Feature, error) {
 	return nil, nil
