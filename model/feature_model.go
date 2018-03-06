4723d73f88c12bfd529fa2d7783b3e8c2e08b8e3
tingold <tingold@boundlessgeo.com>
2018-03-06 14:25:57 -0700

    Merge branch 'master' of https://github.com/boundlessgeo/feshack
    # Conflicts:
    #	handlers/feature_handler.go

---
+++ model/feature_model.go
@@ -0,0 +1,46 @@
+package model
+
+import (
+	"github.com/boundlessgeo/wt/ogc"
+	"fmt"
+)
+
+//creates a feature table based
+func(d *DB) CreateCollectionTable(collectionName string, features []*ogc.Feature) error{
+
+	sql := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (_fid SERIAL UNIQUE, geom geometry(Point,4326),json JSONB)",collectionName)
+	_, err := d.db.Exec(sql)
+	if err != nil {
+		log.Printf("Error creating table: %v",err)
+		return err
+	}
+
+	//makeGeom := "SELECT ST_AddGeometryColumn('public',$1,'geom',4326,'POINT',2)"
+	//_, err = d.db.Exec(makeGeom,collectionName)
+	//if err != nil {
+	//	log.Printf("Error adding geometry column: %v",err)
+	//	return err
+	//}
+	return nil
+
+}
+
+func(d *DB) InsertFeature(collectionName string, features []*ogc.Feature){
+
+	//insert := "INSERT INTO $1 (geom, json) VALUES($2, $3)"
+	//
+	//for _,feature := range features{
+	//
+	//	json, _ := json2.Marshal(feature)
+	//	orb.AllGeometries.
+
+
+	//	}
+
+
+}
+
+//gets features based on query
+func(d *DB) GetFeatures(request ogc.GetFeatureRequest) ([]*ogc.Feature, error) {
+	return nil, nil
+}
