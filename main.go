1f3d4f635ffd84318c3b4c411b921452b2c1006b
Wes Richardet <wes@tetriscodes.com>
2018-03-06 09:14:12 -0700

    Adding table create

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/boundlessgeo/feshack/ogc"
+	"github.com/boundlessgeo/wt/model"
+	"github.com/gin-gonic/gin"
 	"github.com/paulmach/orb"
 )
 
 func main() {
+
+	db := model.NewDB("wfsthree", "wfsthree", "wfsthree", false)
+	var dbErr error
+
+	go func() {
+		dbErr = db.Start()
+
+		if dbErr != nil {
+			log.Panic(dbErr)
+		}
+	}()
+
 	r := gin.Default()
 	r.GET("/test/wfs", func(c *gin.Context) {
 
 		fc := ogc.NewFeatureCollection()
-		p := orb.Point{0,0}
+		p := orb.Point{0, 0}
 		feat := ogc.NewFeature(&p)
 		feat.ID = "testid"
 		feat.Properties["test"] = 1
@@ -19,9 +37,22 @@ func main() {
 
 		c.JSON(200, fc)
 
+	})
 
-		})
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
+	log.Printf("Starting Web Server")
 	r.Run() // listen and serve on 0.0.0.0:8080
-}
 
+	running := true
+	for running == true {
+		select {
+		case sig := <-sigchan:
+			db.Stop(dbErr)
+			log.Printf("Caught signal %v\n", sig)
+			running = false
+		}
+	}
+
+}
